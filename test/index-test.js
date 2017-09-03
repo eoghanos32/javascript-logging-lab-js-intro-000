1039f9343dc6a8f0fe90f8b9967670a2028fad76
Eoghan O'Sullivan <eoghanos32@hotmail.com>
2017-09-03 17:50:41 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -6,10 +6,10 @@ const path = require('path')
 describe('index', () => {
   const html = '<div></div>'
   const src = path.resolve(__dirname, '..', 'index.js')
-console.error("HALP!")
+
   it('calls console.error()', done => {
     const spy = expect.spyOn(console, 'error').andCallThrough()
-
+console.error("HALP!")
     jsdom.env(html, [src], {
       virtualConsole: jsdom.createVirtualConsole().sendTo(console)
     }, (err, window) => {
